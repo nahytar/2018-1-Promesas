9bedb9f0816e3153a3aa13fe19f20493b424b945
nahytar <nahytar@gmail.com>
2018-07-10 00:23:10 -0400

    se realiza movimientos.

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 //La firma es el nombre de la función, los parámetros y lo que retorna
-function animateElement(element, start, target, duration){ //Retornará promesa con elemento
-    element.style.left = start; 
+function animateElement(element, start, target, duration, axis){ //Retornará promesa con elemento
+    element.style[axis] = start; 
     let counter = 0;
-    const delta = (target - start)*40/duration; //delta es lo que se debe mover por cuadro
+    const delta = (target - start) * 40 / duration; //delta es lo que se debe mover por cuadro
     return new Promise((resolve, reject)=>{ // Acá se está declarando la promesa. Los parametros indican lo que resuelven y lo que se rechaza, cuando se resuelve se llama a resolve() y si no se llama a reject()
         const loop = setInterval(()=>{ // toma una funcion y la repite cada ciertos milisegundos
             const current = start + counter++ * delta; //a acá indicamos el movimientoto, ++counter hace que sume y luego se multiplique. Counter ++ suma después. Formula = posición inicial + velocidad*tiempo
-            element.style.left = current;
-            if(start > target && current <= target){ // acá indicamos cuando queremos que finalize el moviento que seria alb llegar a target
-                element.style.left = current;
-                clearInterval(loop); // Acá se termina la promesa
-                resolve();//Si queremos pasar una respuesta es a través del parámetro de resolve
-            }else if(start < target && current >= target){
-                element.style.left = current;
+            element.style[axis] = current;
+            if((start > target && current < target) || (start < target && current > target)){ // acá indicamos cuando queremos que finalize el moviento que seria alb llegar a target
                 clearInterval(loop); // Acá se termina la promesa
                 resolve();//Si queremos pasar una respuesta es a través del parámetro de resolve
             }
@@ -27,30 +22,47 @@ function animateElement(element, start, target, duration){ //Retornará promesa 
 //Secuencial
 
 const allLi = document.getElementsByTagName("li");
-animateElement(allLi[0], -200, 200, 4000).then(()=>{ // coordenadas fuera de la pantalla se indican con numeros negativos. Acá se está haciendo uso de la promesa por fuera.
-    console.log("Terminó la animación de doge");
-    return animateElement(allLi[1], -200, 200, 2000); //" cá tenemos una promesa anidada, es como decirle: hiciste esto, y ahora haz esto otro
-}).then(()=>{ 
-    console.log("Terminó de llegar el cate");
-}).catch(()=>{
-    console.log("Falló la animación");
-});
+
+// animateElement(allLi[0], -200, 200, 4000).then(()=>{ // coordenadas fuera de la pantalla se indican con numeros negativos. Acá se está haciendo uso de la promesa por fuera.
+//     console.log("Terminó la animación de doge");
+//     return animateElement(allLi[1], -200, 200, 2000); //" cá tenemos una promesa anidada, es como decirle: hiciste esto, y ahora haz esto otro
+// }).then(()=>{ 
+//     console.log("Terminó de llegar el cate");
+// }).catch(()=>{
+//     console.log("Falló la animación");
+// });
 
 Promise.all( // esto devuelve un arreglo de promesas y ejecutarlas a la vez, se resuelve cuansdo terminan todas las promesas.
     [
-        animateElement(allLi[1], -200, 600, 8000),
-        animateElement(allLi[0], -200, 600, 4000)
+        animateElement(allLi[1], -200, 600, 9000, 'left'),
+        animateElement(allLi[0], -200, 600, 8000, 'left')
     ]
 ).then((results)=>{
-    console.log("Todas las animaciones terminaron");
+    console.log("terminaron de moverse a la derecha");
     return Promise.all( // esto devuelve un arreglo de promesas y ejecutarlas a la vez, se resuelve cuansdo terminan todas las promesas.
         [
-            animateElement(allLi[1], 600, -200, 8000),
-            animateElement(allLi[0], 600, -200, 4000)
+            animateElement(allLi[0], 0, 400, 9000, 'top'),
+            animateElement(allLi[1], 150, 550, 8000, 'top')
         ]
     )
 }).then(()=>{
-    console.log("Terminaron las animaciones de vuelta");
-}).catch(()=>{
+    console.log("Terminaron de moverse abajo");
+    return Promise.all( // esto devuelve un arreglo de promesas y ejecutarlas a la vez, se resuelve cuansdo terminan todas las promesas.
+        [
+            animateElement(allLi[1], 600, 0, 9000, 'left'),
+        animateElement(allLi[0], 600, 0, 8000, 'left')
+        ]
+    )
+}).then((results)=>{
+    console.log("terminaron de moverse a la izquierda");
+    return Promise.all( // esto devuelve un arreglo de promesas y ejecutarlas a la vez, se resuelve cuansdo terminan todas las promesas.
+        [
+            animateElement(allLi[0], 400, 0, 9000, 'top'),
+            animateElement(allLi[1], 550, 150, 8000, 'top')
+        ] 
+    )
+})
+
+.catch(()=>{
     console.log("Falló la animación");
 });
